1afdc81a3c3865c9051d605d20ad0f2c175c16de
nomin.oyu <nomin.oyu@corp.unitel.mn>
2022-11-28 15:59:03 +0800

    cles

---
+++ JS_basics/area.js
@@ -0,0 +1,5 @@
+//perimeter of traingle
+let aside = ("Enter a side here")
+let bside = ("Enter b side here")
+let cside = ("Enter c side here")
+
--- JS_basics/weather.js
+++ JS_basics/weather.js
@@ -1,18 +1,7 @@
-// // "Comments can make code readable"
-// // "Javascript lesson begin"
-// /* "Comments can make code readable" */
 
-// // Celsius to Fahrenheit
-// let cel = 2;
-// let celtofar = (cel * 9) / 5 + 32;
-// console.log(celtofar);
+// celsius to farenheit 
+let celsius = prompt("Enter Celsius");
+let far = (celsius * 1.8) + 32;
+alert("Celsius to Farenheit: " +  far);
 
-// // Fahrenheit to celsius
-// let far = 1;
-// let fartocel = ((far - 32) * 5) / 9;
-// console.log(fartocel);
 
-let inputNumber = prompt("Enter celsius");
-console.log((inputNumber = cel));
-let Fahrenheit = (cel * 9) / 5 + 32;
-console.log("Fahrenheit is:  " + Fahrenheit);
