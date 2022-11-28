68e7c7310b60880b8b71740214934dd3a868c00a
nomin.oyu <nomin.oyu@corp.unitel.mn>
2022-11-28 18:06:39 +0800

    class

--- JS_basics/weather.js
+++ JS_basics/weather.js
@@ -1,7 +1,32 @@
+// Celsius to Farenheit
+let cel = 2;
+let celtofar = (cel * 9) / 5 + 32;
+console.log(celtofar);
+
+// Farenheit to celsius
+let far = 1;
+let fartocel = ((far - 32) * 5) / 9;
+console.log(fartocel);
 
 // celsius to farenheit 
 let celsius = prompt("Enter Celsius");
 let far = (celsius * 1.8) + 32;
 alert("Celsius to Farenheit: " +  far);
+
+// farenheit to celsius
+let farenheit = prompt("Enter Farenheit");
+let celsius = ((farenheit - 32) * 5) / 9;
+alert("Farenheit to Celsius: " +  celsius);
+
+//perimeter of traingle
+let aside = parseFloat(prompt("Enter a side here"))
+let bside = parseFloat(prompt("Enter b side here"))
+let cside = parseFloat(prompt("Enter c side here"))
+let perimeter = (aside + bside + cside)
+alert ("Perimeter of the triangle is : " + perimeter)
+
+//perimeter of a triangle 
+let aside = 1, bside = 2, cside = 3;
+let.console(aside + bside + cside);
 
 
