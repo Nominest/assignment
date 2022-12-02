25c300a64af59293f120449e94f9fca7bcdbde6a
Morning <morning@MStarss-Anar.local>
2022-12-02 12:34:05 +0800

    a

--- JS_loop/Convert_String/convertstring.js
+++ JS_loop/Convert_String/convertstring.js
@@ -1,4 +1,4 @@
 let str = "enter text here ee";
-let first = str.charAt(0).toLocaleUpperCase() +str.slice(1);
+let first = str.charAt(0).toLocaleUpperCase() + str.slice(1);
 let tail = str.slice(1);
-console.log(first +tail)
+console.log(first + tail);
