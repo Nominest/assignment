933f08f2284d0e1637fcb56c1885efd2dcdb738d
Morning <morning@MStarss-Anar.local>
2022-12-13 12:49:00 +0800

    fix

--- JS_DOM/random_color/function.js
+++ JS_DOM/random_color/function.js
@@ -1,9 +1,8 @@
 function myFunction() {
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        document.body.style.backgroundColor = color;
-      }
-  
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  document.body.style.backgroundColor = color;
+}
