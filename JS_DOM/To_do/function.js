693ab0b4650786a0d889ba42b2e4ea775bfee0d8
Morning <morning@MStarss-Anar.local>
2022-12-15 12:04:32 +0800

    class

---
+++ JS_DOM/To_do/function.js
@@ -0,0 +1,7 @@
+document.getElementById("box").innerHTML =
+  "<div style='color:#1c315e;'> <h2> To-do List App </h2> </div>";
+
+// Гүн цэнхэр: #1c315e;
+// Улбар шар: #f49d1a
+// Саарал 1: #fbf6f6;
+// Саарал 2: #eeeeee;
